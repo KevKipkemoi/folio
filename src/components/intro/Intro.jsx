dd605867e258d5a101298c1078c8cef905d99d60
KevKipkemoi <kevkipkemoi@gmail.com>
2021-06-18 12:37:20 +0300

    profile page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,37 @@
 import "./intro.scss"
+import { init } from "ityped"
+import { useEffect } from "react"
+import { useRef } from "react"
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["Full Stack Developer", "Mobile App Developer"]
+        });
+    }, [])
+
     return (
-        <div className="intro" id="intro"></div>
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imageContainer">
+                    <img src="assets/kev.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h1>Kevin Kipkemoi</h1>
+                    <h3>Freelance <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
+        </div>
     )
 }
